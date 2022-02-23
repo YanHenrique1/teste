0a69b6dd27bf2c0a2d7d9018c878995c49d87e48
YanHenrique1 <Yan_henrique2@outlook.com>
2022-02-23 13:53:46 +0000

    topo

--- prjloja/src/app/app-routing.module.ts
+++ prjloja/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProdutosComponent } from './components/produtos/produtos.component';
+import { TopoComponent } from './components/topo/topo.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'produtos', component: ProdutosComponent},
-  {path: 'cadastro', component: CadastroComponent}
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'topo', component: TopoComponent}
 ];
 
 @NgModule({
